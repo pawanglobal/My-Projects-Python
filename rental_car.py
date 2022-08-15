b070bd87d3d31ca1e9c13f6a1e478055faba818f
PAWAN KUMAR <pawanglobalO7@gmail.com>
2022-08-15 13:46:07 +0200

    To rent a BMW, or AUDIA

---
+++ rental_car.py
@@ -0,0 +1,12 @@
+Rental_Car = input("What kind of rental car you would like to rent?  ")
+
+if Rental_Car == ('BMW'):
+    print(f"\nLet me see if {Rental_Car} is available for rent.")
+    print(f"Yes Sir, we have this car available for rent.")
+    
+elif Rental_Car == ('AUDI'):
+    print(f"\nLet me see if {Rental_Car} is available for rent.")
+    print(f"Yes Sir, we have this car available for rent.")
+    
+else:
+    print("\nSorry Sir, we can't help you, we don't have this car. We only rent BMW, and AUDI.")
