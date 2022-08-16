074959a3470563aa7a45ddbb9e471dda0f4c7f1b
PAWAN KUMAR <pawanglobalO7@gmail.com>
2022-08-16 11:47:50 +0200

    Display t-shirt infromation; using functions

---
+++ t-shirt.py
@@ -0,0 +1,11 @@
+def make_shirt(size= 'Large', logo= 'I Love Python'):
+    """Display t-shirt information"""
+    print(f"\nThe size of the t-shirt is {size.title()}.")
+    print(f"\nThe logo on the t-shirt is {logo.title()}.")
+make_shirt()
+make_shirt(size= 'small', logo= 'adidas')
+make_shirt(size= 'medium')
+make_shirt(size= 'any size', logo= 'humanity')
+
+    
+    
