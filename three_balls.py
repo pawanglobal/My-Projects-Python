49444686b63fdebca49172f73fa4aa3013c5e0f1
pawanglobal <pawanglobal07@gmail.com>
2022-10-07 00:44:00 +0200

    first commit

---
+++ three_balls.py
@@ -0,0 +1,18 @@
+swap = input()
+
+ball_location = 1
+for swap_type in swap:
+    if swap_type == 'A' and ball_location == 1:
+        ball_location = 2
+    elif swap_type == 'A' and ball_location == 2:
+        ball_location = 1
+    elif swap_type == 'B' and ball_location == 2:
+        ball_location = 3
+    elif swap_type == 'B' and ball_location == 3:
+        ball_location = 2
+    elif swap_type == 'C' and ball_location == 3:
+        ball_location = 1
+    elif swap_type == 'C' and ball_location == 1:
+        ball_location = 3
+    
+print(ball_location)
